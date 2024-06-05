c50c9bddf0fd58540a5c7a0f611040fbb52b7856
Kaif <mohammad.kaif@contenterra.com>
2024-06-05 09:42:08 +0530

    pagenation

--- CRUD/All.js
+++ CRUD/All.js
@@ -27,4 +27,27 @@ router.get("/all",jwtVerification, (req, res) => {
   }}
 });
 
+// paginated employees
+
+router.get("/all/:id",jwtVerification, (req, res) => {
+  const id = parseInt(req.params.id);
+  const jsonFilePath = path.join(__dirname, "../DATA/myFiles.json");
+  if (!fs.existsSync(jsonFilePath)) {
+    res.send("Data doesn't exists")
+  } else{
+  const empJSON = require(jsonFilePath);
+  console.log(id);
+
+  if (empJSON) {
+    //pagination for 4 employees
+    const start = (id - 1) * 4;
+    const end = id * 4;
+    const result = empJSON.slice(start, end);
+    res.json(result);
+  } 
+  else {
+    res.send("Employee list empty");
+  }}
+});
+
 module.exports = router;
--- CRUD/Login.js
+++ CRUD/Login.js
@@ -1,33 +1,31 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const router = express.Router()
+const router = express.Router();
 const bcrypt = require("bcrypt");
-const {generateToken} = require('../Auth/generateToken')
+const { generateToken } = require("../Auth/generateToken");
 
 router.post("/", (req, res) => {
   const jsonFilePath = path.join(__dirname, "../DATA/myFiles.json");
   const empJSON = require(jsonFilePath);
   const { email, password } = req.body;
-  console.log(req.body);
   const employee = empJSON.find((ele) => ele.email === email);
-  console.log(employee);
   if (!employee) res.send("Invalid Credentials or Employee doesn't exist");
   bcrypt.compare(password, employee.password, async (err, result) => {
     if (err) {
-        console.error(err);
-        return res.status(500).send("Internal Server Error");
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
     }
     if (!result) {
-        return res.send("Your password is incorrect");
+      return res.send("Your password is incorrect");
     }
     const payload = {
-        email: email,
-        isAdmin:true
-    }
+      email: email,
+      isAdmin: true,
+    };
     const token = generateToken(payload);
-    res.append("jwt_key",token);
-    res.send("Login successful , token generated is " + token)
+    res.append("jwt_key", token);
+    res.send("Login successful , token generated is " + token);
+  });
 });
-})
 module.exports = router;
